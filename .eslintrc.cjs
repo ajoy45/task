0654faf13ddd9b5ce2aa64ea86cf6ce792fc32a4
ajoy45 <ajoypust@gmail.com>
2023-08-03 16:39:25 +0600

    project setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es2021": true
+  },
+  "extends": ["react-app", "react-app/jest", "prettier"],
+  "overrides": [],
+  "parserOptions": {
+    "ecmaVersion": "latest"
+  },
+  "rules": {
+    "no-var": "error",
+    "camelCase": "error",
+    "no-empty": "error",
+    "default-case": "error",
+    "eqeqeq": "error",
+    "max-lines": ["error", 50],
+    "max-depth": ["error", 3]
+  }
+}
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-red-500">hello world</h1>
+      <button className="btn btn-primary">Primary</button>
+    </>
+  )
+}
+
+export default App
