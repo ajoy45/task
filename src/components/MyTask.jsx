c636176df3f85f2a6f0e2960007c190fcc2656c9
ajoy45 <ajoypust@gmail.com>
2023-08-04 16:36:29 +0600

    delete method added

--- src/components/MyTask.jsx
+++ src/components/MyTask.jsx
@@ -1,24 +1,63 @@
 import React, { useEffect, useState } from 'react';
 import MyTaskCard from './MyTaskCard';
+import { toast } from 'react-toastify';
 
 const MyTask = () => {
-    const[myTasks,setMyTAsk]=useState([]);
+    const [myTasks, setMyTAsk] = useState([]);
     console.log(myTasks)
-    useEffect(()=>{
-        fetch('http://localhost:5000/all-task')
+    const handelUpdate = (id) => {
+        console.log(id)
+        fetch(`http://localhost:5000/add-task/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                status: "completed"
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    fetch('http://localhost:5000/all-task')
+                        .then(res => res.json())
+                        .then(data => setMyTAsk(data))
+                        toast('update successfully')
+                }
+            });
+    }
+    const handelDelete = (id) => {
+        console.log(id)
+        fetch(`http://localhost:5000/add-task/${id}`, {
+            method: 'DELETE',
+        })
         .then(res=>res.json())
-        .then(data=>setMyTAsk(data))
-    },[])
+        .then(data=>{
+            if(data.deletedCount>0){
+                fetch('http://localhost:5000/all-task')
+                .then(res => res.json())
+                .then(data => setMyTAsk(data))
+                toast('delete successfully')
+            }
+        })
+    }
+    useEffect(() => {
+        fetch('http://localhost:5000/all-task')
+            .then(res => res.json())
+            .then(data => setMyTAsk(data))
+    }, [])
     return (
-       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-12'>
-        {
-            myTasks.map(myTask=><MyTaskCard
-             key={myTask._id}
-             myTask={myTask}
-            ></MyTaskCard>)
-        }
-       </div>
-       
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-12'>
+            {
+                myTasks.map(myTask => <MyTaskCard
+                    key={myTask._id}
+                    myTask={myTask}
+                    handelUpdate={handelUpdate}
+                    handelDelete={handelDelete}
+                ></MyTaskCard>)
+            }
+        </div>
+
     );
 };
 
--- src/components/MyTaskCard.jsx
+++ src/components/MyTaskCard.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-const MyTaskCard = ({myTask}) => {
-   const{name,price,photoURL,description,status}=myTask;
-   console.log(name,price,photoURL,description,status)
+const MyTaskCard = ({ myTask,handelUpdate,handelDelete }) => {
+    const { _id, name, price, photoURL, description, status } = myTask;
+    console.log(_id, name, price, photoURL, description, status);
+    
+    
     return (
         <div className="card w-full bg-base-100 shadow-2xl ">
             <figure className="px-10 pt-10">
@@ -12,9 +14,10 @@ const MyTaskCard = ({myTask}) => {
                 <h2 className="card-title">Task Name:{name}</h2>
                 <p>{description}</p>
                 <p>price:${price}</p>
+                <p>status:{status}</p>
                 <div className="card-actions">
-                    <button className="px-4 py-2 rounded-lg bg-[#4070ff] text-white">Update</button>
-                    <button className=" px-4 py-2 rounded-lg bg-[#4070ff] text-white">Delete</button>
+                    <button onClick={() => handelUpdate(_id)} className="px-4 py-2 rounded-lg bg-[#4070ff] text-white">Update</button>
+                    <button onClick={()=>handelDelete(_id)} className=" px-4 py-2 rounded-lg bg-[#4070ff] text-white">Delete</button>
                 </div>
             </div>
         </div>
